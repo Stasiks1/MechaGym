16b71df99804e3852d90ee6cdec1302f8d207909
Stasiks <stanislavvv2011@gmail.com>
2026-09-07 21:44:55 +0300

    Refactor main logic and add workspace configuration

--- src/main.cpp
+++ src/main.cpp
@@ -1,67 +1,41 @@
 #include <Arduino.h>
 #include <M5StickCPlus.h>
 
+int gameState = 0;
+unsigned long waitStart = 0;
+unsigned long randomDelay = 0;
+unsigned long reactionStart = 0;
+
 int speed = 0;
-void updateDashboard() {
-  M5.Lcd.fillScreen(BLACK);
-  if (speed >= 80) {
-    M5.Lcd.setTextColor(RED, BLACK);
-  }
-    else {
-    M5.Lcd.setTextColor(GREEN, BLACK);
-     }
-  M5.Lcd.setTextSize(4);
-  M5.Lcd.setCursor(10,10);
-  M5.Lcd.print(speed);
-  if (speed == 0) {
-    M5.Lcd.setTextColor(BLUE, BLACK);
-    M5.Lcd.setTextSize(5);
-    M5.Lcd.setCursor(10, 50);
-    M5.Lcd.print("PARKED");
-  }
-    else if (speed > 0 && speed < 80) {
-    M5.Lcd.setTextColor(GREEN, BLACK);
-    M5.Lcd.setTextSize(5);
-    M5.Lcd.setCursor(10, 50);
-    M5.Lcd.print("DRIVE");
-    }
-    else if (speed >= 80) {
-    M5.Lcd.setTextColor(RED, BLACK);
-    M5.Lcd.setTextSize(4);
-    M5.Lcd.setCursor(10, 50);
-    M5.Lcd.print("OVERHEAT!");
-    }
-    }
+
 
 void setup() {
   M5.begin();
   M5.Lcd.setRotation(3);
-  M5.Lcd.fillScreen(BLACK);
-  M5.Lcd.setTextColor(GREEN, BLACK);
+  M5.Lcd.fillScreen(BLUE);
+  M5.Lcd.setTextColor(GREEN, BLUE);
   M5.Lcd.setTextSize(2);
-  M5.Lcd.setCursor(10,10);
-  M5.Lcd.print("Hello, User!");
+  M5.Lcd.setCursor(20,50);
+  M5.Lcd.print("PRESS A TO START!");
+
 }
 
 void loop() {
   M5.update();
   M5.Beep.update();
+  switch (gameState) {
+  case 0:
   if (M5.BtnA.wasPressed()) {
-      speed += 10;
-      if (speed > 100) {
-      speed = 100;
-      }
-      M5.Beep.tone(3500, 30);
-      updateDashboard();
-    
+    M5.Lcd.fillScreen(RED);
+    M5.Lcd.setCursor(10, 50);
+    M5.Lcd.setTextColor(WHITE, RED);
+    M5.Lcd.setTextSize(3);
+    M5.Lcd.print("WAIT!");
+    randomDelay = random(2000, 5000);
+    waitStart = millis();
+    gameState = 1;
   }
-  if (M5.BtnB.wasPressed()) {
-      speed -= 10;
-      if (speed < 0) {
-      speed = 0;
-    }
-    M5.Beep.tone(1500, 30);
-    updateDashboard();
-  }
+  break;
+  case 1:{};
 }
-
+}
