bc7ffa08c55d29b53063c5ddfd6c5682f37536c0
Stasiks <stanislavvv2011@gmail.com>
2026-09-08 20:38:43 +0300

    Enhance game logic with additional feedback for reaction times

--- src/main.cpp
+++ src/main.cpp
@@ -36,6 +36,60 @@ void loop() {
     gameState = 1;
   }
   break;
-  case 1:{};
+  case 1:
+  if (M5.BtnA.wasPressed()) {
+    M5.Lcd.fillScreen(MAGENTA);
+    M5.Lcd.setCursor(10,50);
+    M5.Lcd.setTextColor(RED, MAGENTA);
+    M5.Lcd.setTextSize(3);
+    M5.Lcd.print("FALSE START!");
+    M5.Beep.tone(1000, 200);
+    delay(1500);
+    gameState = 0;
+  }
+  if (millis() - waitStart >= randomDelay){
+    M5.Lcd.fillScreen(GREEN);
+    M5.Lcd.setCursor(10,50);
+    M5.Lcd.setTextColor(RED, GREEN);
+    M5.Lcd.setTextSize(3);
+    M5.Lcd.print("FIRE! PRESS A!");
+    M5.Beep.tone(4000, 60);
+    reactionStart = millis();
+    gameState = 2;
+  }
+  break;
+  case 2:
+  if (M5.BtnA.wasPressed()) {
+    unsigned long reactionTime = millis() - reactionStart;
+    M5.Lcd.fillScreen(BLACK);
+    M5.Lcd.setCursor(10, 20);
+    M5.Lcd.setTextSize(3);
+    M5.Lcd.setTextColor(YELLOW, BLACK);
+    M5.Lcd.printf("%lu ms", reactionTime);
+  
+  if (reactionTime < 200) {
+    M5.Lcd.fillScreen(BLACK);
+    M5.Lcd.setCursor(10, 50);
+    M5.Lcd.setTextSize(3);
+    M5.Lcd.setTextColor(GREEN, BLACK);
+    M5.Lcd.print("CYBER GOD!");
+  }
+  else if (reactionTime < 320 && reactionTime > 200) {
+    M5.Lcd.fillScreen(BLACK);
+    M5.Lcd.setCursor(10, 50);
+    M5.Lcd.setTextSize(3);
+    M5.Lcd.setTextColor(YELLOW, BLACK);
+    M5.Lcd.print("FAST!");
+  }
+  else {
+    M5.Lcd.fillScreen(BLACK);
+    M5.Lcd.setCursor(10, 50);
+    M5.Lcd.setTextSize(3);
+    M5.Lcd.setTextColor(RED, BLACK);
+    M5.Lcd.print("TOO SLOW!");
+  }
+  gameState = 0;
+  }
+break;
 }
 }
